cb5ec8537cf4d84c717914fde3f08cb7871a0173
vijayjavaprgmr <vijayjavaprgmr@gmail.com>
2021-01-07 14:45:39 +0530

    calculator should be able to add two numbers

---
+++ src/main/java/com/bnpp/kata/Calculator.java
@@ -0,0 +1,9 @@
+package com.bnpp.kata;
+
+public class Calculator {
+
+	public Integer twoNumbersAddition(int i, int j) {
+		return (i + j);
+	}
+
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,23 @@
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.bnpp.kata.Calculator;
+
+public class CalculatorTest {
+
+	Calculator calculatorObj = null;
+
+	@Before
+	public void initializeCalculatorObj() {
+		calculatorObj = new Calculator();
+	}
+
+	@Test
+	public void twoNumbersAdditionTest() {
+		assertThat(calculatorObj.twoNumbersAddition(5, 5), is(10));
+	}
+
+}
